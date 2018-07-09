4f477f6a56cf551aeefea2bae25d234a6aeb12ac
tbirdso <40648863+tbirdso@users.noreply.github.com>
2018-07-09 19:47:26 -0400

    Experiments and Hitboxes
    Added file reading script (unused)
    Added stanchion interactability
    Added stanchion usability
    Adjusted rigidbodies and colliders for PCs and tables

---
+++ RoomSimulator/Assets/Scripts/ReadData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+
+public class ReadData : MonoBehaviour {
+
+	public string file;
+
+	// Use this for initialization
+	void Start () {
+		Load (file);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private bool Load(string fileName) {
+		try {
+			string line;
+			StreamReader theReader = new StreamReader(fileName, Encoding.Default);
+			using(theReader) {
+				do {
+					line = theReader.ReadLine();
+					if(line!=null) {
+						Debug.Log(line);
+						/*string[] entries = line.Split(' ');
+						if(entries.Length > 0) {
+							
+						}*/
+					} 
+				} while (line != null);
+
+				theReader.Close();
+				return true;
+			}
+		} catch (IOException e) {
+			Debug.Log(e.Message);
+			return false;
+		} 
+	}
+}
---
+++ RoomSimulator/Assets/Scripts/move_rope.cs
@@ -0,0 +1,39 @@
+﻿namespace VRTK {
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+		public class move_rope : VRTK_InteractableObject {
+
+		private bool ropeIsOn = false;
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+
+			public override void StartUsing(VRTK_InteractUse usingObject)
+			{
+				Debug.Log ("I am using!");
+				base.StartUsing(usingObject);
+				toggleRope ();
+			}
+
+		public void toggleRope() {
+			GameObject ropeObj = transform.Find ("rope").gameObject;
+			if (ropeObj) {
+				Debug.Log ("Found the rope on this stanchion!");
+				if (!ropeIsOn) {
+					ropeObj.SetActive (true);
+					ropeIsOn = true;
+				} else {
+					ropeObj.SetActive(false);
+					ropeIsOn = false;
+				}
+			} else {
+				Debug.Log ("Attempted to toggle but rope was not found.");
+			}
+
+		}
+	}
+}
