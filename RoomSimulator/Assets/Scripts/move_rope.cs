d0cf355c9864de200212ef592fccb64f8df328f8
tbirdso <40648863+tbirdso@users.noreply.github.com>
2018-07-11 22:41:54 -0400

    General Updates
    Updated workstation 1 hierarchy to exclude Touch controllers from dependency on table rigidbody
    Updated stanchion script to detect initial state
    Added window frames for east and west walls
    Updated build files

--- RoomSimulator/Assets/Scripts/move_rope.cs
+++ RoomSimulator/Assets/Scripts/move_rope.cs
@@ -5,11 +5,12 @@
 
 	public class move_rope : VRTK_InteractableObject {
 
-		private bool ropeIsOn = false;
+		private bool ropeIsOn;
 
 		// Use this for initialization
 		void Start () {
-			
+			GameObject ropeObj = transform.Find ("rope").gameObject;
+			ropeIsOn = ropeObj.activeSelf ;
 		}
 
 		public override void StartUsing(VRTK_InteractUse usingObject)
