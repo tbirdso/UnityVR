9596c678f09a2786397aa7c76ba7a99687f7a19e
tbirdso <40648863+tbirdso@users.noreply.github.com>
2018-07-18 15:33:13 -0400

    Shadows and Glass
    Updated shadow script to control vertical movement from a localized perspective
    Created better glass window texture
    Added basic ceiling and adjusted lighting

--- RoomSimulator/Assets/Scripts/ShadowMove.cs
+++ RoomSimulator/Assets/Scripts/ShadowMove.cs
@@ -5,13 +5,24 @@
 
 	public class ShadowMove : VRTK_InteractableObject {
 
-		private string shadowTag = "Shadow";
+		public Transform InteractShadow;
+
+		private float initShadowY = 0;
+		private float initTableY = 0;
 
 		// Use this for initialization
 		/*override void Start () {
 			base.Start ();
 			Debug.Log ("Started.");
 		}*/
+
+		new void Update () {
+			base.Update ();
+
+			if (InteractShadow.gameObject.activeSelf == true) {
+				shadowFollow ();
+			}
+		}
 
 		public override void Grabbed(VRTK_InteractGrab usingObject)
 		{
@@ -27,23 +38,33 @@
 
 		public void makeShadow() {
 			foreach(Transform t in transform) {
-				if (t.tag.ToString().Equals(shadowTag)) {
-					t.gameObject.SetActive(true);
-					t.gameObject.GetComponent<MeshCollider> ().enabled = false;
-				} else {
-					t.gameObject.SetActive(false);
-				}
+				t.gameObject.SetActive(false);
 			}
 
+			InteractShadow.gameObject.SetActive (true);
+			InteractShadow.gameObject.GetComponent<MeshCollider> ().enabled = false;
+
+			initTableY = transform.position.y;
 		}
 
 		private void makeVisible() {
-			foreach(Transform t in transform) {
-				if (t.tag == shadowTag) {
-					t.gameObject.SetActive(false);					
-				} else {
-					t.gameObject.SetActive(true);
-				}
+
+			Vector3 adjPos = transform.position;
+			adjPos.y = initTableY;
+			transform.SetPositionAndRotation (adjPos, transform.rotation);			
+
+			foreach (Transform t in transform) {
+				t.gameObject.SetActive (true);
+			}
+				
+			InteractShadow.gameObject.SetActive (false);
+		}
+
+		private void shadowFollow() {
+			if (InteractShadow.position.y != initShadowY) {
+				Vector3 shadowPos = InteractShadow.position;
+				shadowPos.y = initShadowY;
+				InteractShadow.SetPositionAndRotation (shadowPos, InteractShadow.rotation);
 			}
 		}
 	}
